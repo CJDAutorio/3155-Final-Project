33247c3adccdd1a642455ab9d78ad74f44fe51d8
g445g445 <bebpower@gmail.com>
2021-04-25 16:13:05 -0400

    test

--- Python/3155BackToTheSkies/Plots/MostVac.py
+++ Python/3155BackToTheSkies/Plots/MostVac.py
@@ -10,11 +10,13 @@ import plotly.offline as pyo
 data1 = pd.read_csv('../Datasets/WHO-COVID-19-global-data.csv')
 data2 = pd.read_csv('../Datasets/vaccination-data.csv')
 
+# --------------------------------
 # BAR CHART Most Vaccinated Countries
 # --------------------------------
+
 # Filtering data
 dataVacPerCap = data2.sort_values(by=['TOTAL_VACCINATIONS_PER100'], ascending=[False]).head(10)
-dataVacPerCap = dataVacPerCap.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+dataVacPerCap = dataVacPerCap.apply(lambda x: x.str.strip() if x.dtype == "object" else x)  # Removing empty spaces
 
 # Preparing data and layout
 dataPer100 = [go.Bar(x=dataVacPerCap['COUNTRY'], y=dataVacPerCap['TOTAL_VACCINATIONS_PER100'] / 100)]
@@ -24,5 +26,29 @@ layoutPer100 = go.Layout(title="Most Vaccinated Counties", title_font_size=22, x
 fig = go.Figure(data=dataPer100, layout=layoutPer100)
 pyo.plot(fig, filename='VacPer100barchart.html')
 
+# --------------------------------
 # STACKED BAR CHART Most Tourism
 # --------------------------------
+
+# Filtering data
+dataMostTourism = data2[data2['ISO3'] == ('FRA', 'ESP', 'USA')]
+dataMostTourism = dataMostTourism.apply(lambda x: x.str.strip() if x.dtype == "object" else x)  # Removing empty spaces
+# Preparing data and layout
+
+# Plot the figure and saving in a html file
+
+
+# --------------------------------
+# STACKED BAR CHART Most Tourism
+# --------------------------------
+
+# Filtering data
+
+# Preparing data and layout
+dataFin = [go.Bar(x=dataMostTourism['COUNTRY'], y=dataMostTourism['TOTAL_VACCINATIONS_PER100'] / 100)]
+layout1 = go.Layout(title="Most Vaccinated Counties", title_font_size=22, xaxis_title="Country",
+                         yaxis_title="Percentage Vaccinated")
+
+# Plot the figure and saving in a html file
+fig = go.Figure(data=dataFin, layout=layout1)
+pyo.plot(fig, filename='barchart.html')
