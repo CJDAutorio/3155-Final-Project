89642571b35468e7b64f7e59414fb03dcadb14b3
g445g445 <bebpower@gmail.com>
2021-04-25 15:14:27 -0400

    Update MostVac.py

--- Python/3155BackToTheSkies/Plots/MostVac.py
+++ Python/3155BackToTheSkies/Plots/MostVac.py
@@ -14,9 +14,11 @@ data2 = pd.read_csv('../Datasets/vaccination-data.csv')
 dataVacPerCap = data2.sort_values(by=['TOTAL_VACCINATIONS_PER100'], ascending=[False]).head(10)
 
 # Preparing data
-
+dataPer100 = [go.Bar(x=dataVacPerCap['COUNTRY'], y=dataVacPerCap['TOTAL_VACCINATIONS_PER100'])]
 
 # Preparing layout
-
+layoutPer100 = go.Layout(title="Vac per 100", xaxis_title="Country", yaxis_title="Total per 100")
 
 # Plot the figure and saving in a html file
+fig = go.Figure(data=dataPer100, layout=layoutPer100)
+pyo.plot(fig, filename='barchart.html')
