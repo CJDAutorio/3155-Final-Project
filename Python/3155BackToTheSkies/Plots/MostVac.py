8a24ca8b4534eab52680fe40c36510b787749914
g445g445 <bebpower@gmail.com>
2021-04-25 18:34:58 -0400

    Pre Testing for DCC/dash

--- Python/3155BackToTheSkies/Plots/MostVac.py
+++ Python/3155BackToTheSkies/Plots/MostVac.py
@@ -11,22 +11,6 @@ data1 = pd.read_csv('../Datasets/WHO-COVID-19-global-data.csv')
 data2 = pd.read_csv('../Datasets/vaccination-data.csv')
 
 # --------------------------------
-# BAR CHART Most Vaccinated Countries
-# --------------------------------
-
-# --- Filtering data ---
-dataVacPerCap = data2.sort_values(by=['TOTAL_VACCINATIONS_PER100'], ascending=[False]).head(10)
-dataVacPerCap = dataVacPerCap.apply(lambda x: x.str.strip() if x.dtype == "object" else x)  # Removing empty spaces
-
-# --- Preparing data and layout ---
-dataPer100 = [go.Bar(x=dataVacPerCap['COUNTRY'], y=dataVacPerCap['TOTAL_VACCINATIONS_PER100'] / 100)]
-layoutPer100 = go.Layout(title="Most Vaccinated Counties", title_font_size=22, xaxis_title="Country",
-                         yaxis_title="Percentage Vaccinated")
-# --- Plot the figure and saving in a html file ---
-fig = go.Figure(data=dataPer100, layout=layoutPer100)
-# pyo.plot(fig, filename='VacPer100barchart.html') ---UNCOMENT WHEN DONE
-
-# --------------------------------
 # STACKED BAR CHART Most Tourism
 # --------------------------------
 
@@ -53,15 +37,9 @@ layout1 = go.Layout(title="Most Vaccinated Counties", title_font_size=22, xaxis_
                     yaxis_title="Percentage Vaccinated")
 
 # --- Plot the figure and saving in a html file ---
+dcc.Graph
+
 fig = go.Figure(data=data_most_tour, layout=layout1)
 pyo.plot(fig, filename='barchart.html')
 
-# --------------------------------
-# STACKED BAR CHART Most Tourism
-# --------------------------------
 
-# --- Filtering data ---
-
-# --- Preparing data and layout ---
-
-# --- Plot the figure and saving in a html file ---
