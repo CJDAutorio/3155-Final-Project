3606ed17dee4f00159b7c9747ccb0ccdc0e1df72
CJDAutorio <cj.dautorio2021@gmail.com>
2021-04-27 18:20:09 -0400

    Header and Button Layouts Began work on content

--- Python/3155BackToTheSkies/SiteFramework/Home.py
+++ Python/3155BackToTheSkies/SiteFramework/Home.py
@@ -14,15 +14,24 @@ app.layout = html.Div(children=[
                 dbc.Col(html.Div(html.H1("Back to the Skies")), width=4),
                 dbc.Col(html.Div(html.H2("COVID-19 Vaccination Data for Airline Travel")), width="auto")
             ], align="start")
-            ),
+        ),
         # Nav Buttons
-        dbc.Row(children=[
-            dbc.Col(html.Div(html.A("Point to Point", href="https://www.google.com/")), width=4),
-            dbc.Col(html.Div(html.A("See the Data", href="https://www.google.com/")), width=4),
-            dbc.Col(html.Div(html.A("About Us", href="https://www.google.com/")), width=4)
-        ], align="center")
-    ])
+        html.Div(children=[
+            dbc.Row(children=[
+                dbc.Col(html.Div(html.A("Point to Point", href="https://www.google.com/")), width=4),
+                dbc.Col(html.Div(html.A("See the Data", href="https://www.google.com/")), width=4),
+                dbc.Col(html.Div(html.A("About Us", href="https://www.google.com/")), width=4)
+            ])
+        ])
+    ]),
 
+    # Body
+    html.Div(children=[
+        dbc.Row(children=[
+            dbc.Col(html.Div(style={"background-color": "#000000", "height": 500, "width": 500})),
+            dbc.Col(html.Div(html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")))
+        ])
+    ])
 ])
 
 if __name__ == "__main__":
