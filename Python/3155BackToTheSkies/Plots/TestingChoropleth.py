c60d5e7481364a4ed962b805ea67d5dec8662173
g445g445 <bebpower@gmail.com>
2021-04-27 14:26:43 -0400

    Learning chloro

--- Python/3155BackToTheSkies/Plots/TestingChoropleth.py
+++ Python/3155BackToTheSkies/Plots/TestingChoropleth.py
@@ -22,8 +22,17 @@ usa_data = data1
 usa_data['Week of Allocations'] = pd.to_datetime(usa_data['Week of Allocations'])
 usa_dataClean = usa_data.sort_values(by=['Week of Allocations'], ascending=[False]).head(10)
 
-
 # --- Preparing data and layout ---
-print(usa_dataClean['Week of Allocations'])
-print(pd.to_datetime(2020-12-21))
+fig = px.choropleth(usa_dataClean, locations='Jurisdiction', locationmode='USA-states', color="1st Dose Allocations",
+                    hover_name='Jurisdiction', projection="albers usa", title="temp",
+                    color_continuous_scale=px.colors.sequential.Agsunset)
+
 # --- Plot the figure and saving in a html file ---
+app.layout = html.Div([
+    html.Div([
+        dcc.Graph(figure=fig)
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
