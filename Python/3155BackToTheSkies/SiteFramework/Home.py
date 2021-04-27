ce3b8f92f9d6a55f4a5155ea96772453c9cbe904
CJDAutorio <cj.dautorio2021@gmail.com>
2021-04-27 18:02:41 -0400

    Header and Button Layouts

--- Python/3155BackToTheSkies/SiteFramework/Home.py
+++ Python/3155BackToTheSkies/SiteFramework/Home.py
@@ -6,21 +6,24 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div(
-    children=[
-        # Header
-        dbc.Row(children=[
+app.layout = html.Div(children=[
+    # Header
+    html.Div(children=[
+        html.Div(
+            dbc.Row(children=[
                 dbc.Col(html.Div(html.H1("Back to the Skies")), width=4),
-                dbc.Col(html.Div(html.H2("COVID-19 Vaccination Data for Airline Travel")), width=8)
-            ]),
+                dbc.Col(html.Div(html.H2("COVID-19 Vaccination Data for Airline Travel")), width="auto")
+            ], align="start")
+            ),
         # Nav Buttons
         dbc.Row(children=[
-                dbc.Col(html.Div(html.P("Point to Point")), width=4),
-                dbc.Col(html.Div(html.P("See the Data")), width=4),
-                dbc.Col(html.Div(html.P("About Us")), width=4)
-            ])
-    ]
-)
+            dbc.Col(html.Div(html.A("Point to Point", href="https://www.google.com/")), width=4),
+            dbc.Col(html.Div(html.A("See the Data", href="https://www.google.com/")), width=4),
+            dbc.Col(html.Div(html.A("About Us", href="https://www.google.com/")), width=4)
+        ], align="center")
+    ])
+
+])
 
 if __name__ == "__main__":
     app.run_server()
