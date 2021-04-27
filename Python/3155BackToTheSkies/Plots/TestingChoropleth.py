7da7e48624918cbda49a6b2ecefe108cb06c094c
g445g445 <bebpower@gmail.com>
2021-04-27 15:08:35 -0400

    Pre Testing for DCC/dash

--- Python/3155BackToTheSkies/Plots/TestingChoropleth.py
+++ Python/3155BackToTheSkies/Plots/TestingChoropleth.py
@@ -9,7 +9,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 # Load CSV files from Dataset folder
-data1 = pd.read_csv('../Datasets/COVID-19_Vaccine_Distribution_Allocations_by_Jurisdiction_-_Moderna.csv')
+data1 = pd.read_csv('../Datasets/COVID-19_Vaccine_Distribution_Allocations_by_Jurisdiction_-_ModernaWOW.csv')
 
 # --------------------------------
 # STACKED BAR CHART Most Tourism
@@ -20,7 +20,28 @@ app = dash.Dash()
 # --- Filtering data ---
 usa_data = data1
 usa_data['Week of Allocations'] = pd.to_datetime(usa_data['Week of Allocations'])
-usa_dataClean = usa_data.sort_values(by=['Week of Allocations'], ascending=[False]).head(10)
+usa_dataClean = usa_data.sort_values(by=['Week of Allocations'], ascending=[False]).head(60)
+
+state_codes = {
+    'District of Columbia': 'dc', 'Mississippi': 'MS', 'Oklahoma': 'OK',
+    'Delaware': 'DE', 'Minnesota': 'MN', 'Illinois': 'IL', 'Arkansas': 'AR',
+    'New Mexico': 'NM', 'Indiana': 'IN', 'Maryland': 'MD', 'Louisiana': 'LA',
+    'Idaho': 'ID', 'Wyoming': 'WY', 'Tennessee': 'TN', 'Arizona': 'AZ',
+    'Iowa': 'IA', 'Michigan': 'MI', 'Kansas': 'KS', 'Utah': 'UT',
+    'Virginia': 'VA', 'Oregon': 'OR', 'Connecticut': 'CT', 'Montana': 'MT',
+    'California': 'CA', 'Massachusetts': 'MA', 'West Virginia': 'WV',
+    'South Carolina': 'SC', 'New Hampshire': 'NH', 'Wisconsin': 'WI',
+    'Vermont': 'VT', 'Georgia': 'GA', 'North Dakota': 'ND',
+    'Pennsylvania': 'PA', 'Florida': 'FL', 'Alaska': 'AK', 'Kentucky': 'KY',
+    'Hawaii': 'HI', 'Nebraska': 'NE', 'Missouri': 'MO', 'Ohio': 'OH',
+    'Alabama': 'AL', 'Rhode Island': 'RI', 'South Dakota': 'SD',
+    'Colorado': 'CO', 'New Jersey': 'NJ', 'Washington': 'WA',
+    'North Carolina': 'NC', 'New York': 'NY', 'Texas': 'TX',
+    'Nevada': 'NV', 'Maine': 'ME', 'Palau': 'RIP',"Federal Entities": 'RIP', 'Philadelphia': 'RIP', 'Chicago': 'RIP'}
+
+# usa_dataClean['Jurisdiction'] = usa_dataClean[usa_dataClean['Jurisdiction'] == state_codes]
+usa_dataClean = usa_dataClean.apply(lambda x: x.str.strip() if x.dtype == "object" else x)  # Removing empty spaces
+usa_dataClean['Jurisdiction'] = usa_dataClean['Jurisdiction'].apply(lambda x: state_codes[x])
 
 # --- Preparing data and layout ---
 fig = px.choropleth(usa_dataClean, locations='Jurisdiction', locationmode='USA-states', color="1st Dose Allocations",
@@ -36,4 +57,3 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
