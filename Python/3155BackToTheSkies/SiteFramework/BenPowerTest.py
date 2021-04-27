f4a8cfef3223a559bfcc9432748d757109caa542
g445g445 <bebpower@gmail.com>
2021-04-27 16:40:26 -0400

    Pre Testing for DCC/dash

--- Python/3155BackToTheSkies/SiteFramework/BenPowerTest.py
+++ Python/3155BackToTheSkies/SiteFramework/BenPowerTest.py
@@ -1,12 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = dbc.Container(
-    dbc.Alert("Hello Bootstrap!", color="success"),
-    className="p-5",
-)
+
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",
@@ -24,7 +24,30 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem",
 }
 
+sidebar = html.Div(
+    [
+        html.H2("Sidebar",className="display-4")
+        html.hr(),
+        html.P(
+            "Sidebar with nav links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Home", href="/page-1", active="exact"),
+                dbc.NavLink("Home", href="/page-2", active="exact"),
+            ],
+            vertical = True,
+            pills = True,
+        )
+    ]
+)
 
+app.layout = dbc.Container(
+    dbc.Alert("Hello Bootstrap!", color="success"),
+
+    className="p-5",
+)
 
 if __name__ == "__main__":
     app.run_server()
