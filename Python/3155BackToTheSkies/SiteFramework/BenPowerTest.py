c6aa91069a8f3fffea95d9538f2f5674f6339552
g445g445 <bebpower@gmail.com>
2021-04-27 16:33:05 -0400

    Pre Testing for DCC/dash

--- Python/3155BackToTheSkies/SiteFramework/BenPowerTest.py
+++ Python/3155BackToTheSkies/SiteFramework/BenPowerTest.py
@@ -7,6 +7,24 @@ app.layout = dbc.Container(
     dbc.Alert("Hello Bootstrap!", color="success"),
     className="p-5",
 )
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+}
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+
 
 if __name__ == "__main__":
     app.run_server()
