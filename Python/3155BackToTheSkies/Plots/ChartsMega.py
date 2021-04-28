8f6ee29a07ebf45cddf10e3c9af6754913985f03
g445g445 <bebpower@gmail.com>
2021-04-28 15:29:30 -0400

    - Moved testing files around - Started charts mega script for final implementation of website

--- Python/3155BackToTheSkies/Plots/ChartsMega.py
+++ Python/3155BackToTheSkies/Plots/ChartsMega.py
@@ -6,7 +6,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-# Load CSV files from Dataset folder
+# --- Load CSV files from Dataset folder ---
 data_covid_global = pd.read_csv('../Datasets/WHO-COVID-19-global-data.csv')
-data_vax_global = pd.read_csv('')
-data_vax_usaDist = pd.read_csv('')
+data_vax_global = pd.read_csv('../Datasets/vaccination-data.csv')
+data_vax_usaDist = pd.read_csv('../Datasets/COVID-19_Vaccine_Distribution_Allocations_by_Jurisdiction_-_ModernaWOW.csv')
+
+
+
