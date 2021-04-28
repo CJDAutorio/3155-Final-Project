62598bf2c98a79ac7f7284a4d6f29ede6260763e
g445g445 <bebpower@gmail.com>
2021-04-28 18:41:10 -0400

    Update ChartsMega.py

--- Python/3155BackToTheSkies/Plots/ChartsMega.py
+++ Python/3155BackToTheSkies/Plots/ChartsMega.py
@@ -16,7 +16,6 @@ data_vax_usaDist = pd.read_csv('../Datasets/COVID-19_Vaccine_Distribution_Alloca
 # BAR CHART Most Vaccinated Countries
 # --------------------------------
 def bar1_most_vax():
-
     # --- Filtering data ---
     # Sorts data by TOTAL_VACCINATIONS_PER100 then removes all but the top 10
     data_most_vax = data_vax_global.sort_values(by=['TOTAL_VACCINATIONS_PER100'], ascending=[False]).head(10)
@@ -34,11 +33,11 @@ def bar1_most_vax():
     fig = go.Figure(data=graph_most_vax, layout=layout_most_vax)  # figure used by dash to make chart
     return fig
 
+
 # --------------------------------
 # BAR CHART Least Vaccinated Countries
 # --------------------------------
 def bar2_least_vax():
-
     # --- Filtering data ---
     # removes countries that haven't started vaccinations
     data_least_vax = data_vax_global[data_vax_global['TOTAL_VACCINATIONS_PER100'] > 1]
@@ -58,11 +57,11 @@ def bar2_least_vax():
     fig = go.Figure(data=graph_least_vax, layout=layout_least_vax)  # figure used by dash to make chart
     return fig
 
+
 # --------------------------------
 # STACKED BAR CHART Most Tourism Vax Data
 # --------------------------------
 def sbar1_most_tour():
-
     # --- Filtering data ---
     country_code = ['ESP', 'USA', 'ITA', 'TUR', 'MEX', 'DEU', 'THA', 'GBR', 'JPN']
     data_most_tourism = data_vax_global[data_vax_global['ISO3'] == 'FRA']
@@ -70,10 +69,6 @@ def sbar1_most_tour():
         data_most_tourism = data_most_tourism.append(data_vax_global[data_vax_global['ISO3'] == x])
     data_most_tourism = data_most_tourism.apply(lambda x: x.str.strip if x.dtype == "object" else x)
 
-
     # --- Preparing data and layout ---
 
     # --- Plot the figure and saving in a html file ---
-
-
-
