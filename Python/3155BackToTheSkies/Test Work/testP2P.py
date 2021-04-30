15a56bff5de8e3837a15e11c7bb57fee94a33a28
g445g445 <bebpower@gmail.com>
2021-04-30 14:53:26 -0400

    Started MVP Of p2p

---
+++ Python/3155BackToTheSkies/Test Work/testP2P.py
@@ -0,0 +1,49 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
+
+app = dash.Dash()
+
+data_usa_cases = pd.read_csv('../Datasets/United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv')
+state_codes = {
+    'District of Columbia': 'dc', 'Mississippi': 'MS', 'Oklahoma': 'OK',
+    'Delaware': 'DE', 'Minnesota': 'MN', 'Illinois': 'IL', 'Arkansas': 'AR',
+    'New Mexico': 'NM', 'Indiana': 'IN', 'Maryland': 'MD', 'Louisiana': 'LA',
+    'Idaho': 'ID', 'Wyoming': 'WY', 'Tennessee': 'TN', 'Arizona': 'AZ',
+    'Iowa': 'IA', 'Michigan': 'MI', 'Kansas': 'KS', 'Utah': 'UT',
+    'Virginia': 'VA', 'Oregon': 'OR', 'Connecticut': 'CT', 'Montana': 'MT',
+    'California': 'CA', 'Massachusetts': 'MA', 'West Virginia': 'WV',
+    'South Carolina': 'SC', 'New Hampshire': 'NH', 'Wisconsin': 'WI',
+    'Vermont': 'VT', 'Georgia': 'GA', 'North Dakota': 'ND',
+    'Pennsylvania': 'PA', 'Florida': 'FL', 'Alaska': 'AK', 'Kentucky': 'KY',
+    'Hawaii': 'HI', 'Nebraska': 'NE', 'Missouri': 'MO', 'Ohio': 'OH',
+    'Alabama': 'AL', 'Rhode Island': 'RI', 'South Dakota': 'SD',
+    'Colorado': 'CO', 'New Jersey': 'NJ', 'Washington': 'WA',
+    'North Carolina': 'NC', 'New York': 'NY', 'Texas': 'TX',
+    'Nevada': 'NV', 'Maine': 'ME', 'Palau': 'RIP', "Federal Entities": 'RIP', 'Philadelphia': 'RIP',}
+
+states = list(state_codes.keys())
+
+app.layout = html.Div(children=[
+    html.H1(children='Python Dash',
+            style={
+                'textAlign': 'center',
+                'color': '#ef3e18'
+            }
+            ),
+    html.Div('Please select a continent', style={'color': '#ef3e18', 'margin': '10px'}),
+    dcc.Dropdown(
+        id='select-country',
+        options=[
+            {'label': state, 'value': state} for state in states
+        ],
+        value=list(state_codes.keys())[0]
+    ),
+])
+
+
+if __name__ == '__main__':
+    app.run_server()
